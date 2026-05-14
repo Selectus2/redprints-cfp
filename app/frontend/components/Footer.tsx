e6143c08d404edb16f134b8abf462cf32854bf02
Vishwajeetsingh Desurkar <vishwajeetsinghd@gmail.com>
2026-05-14 15:48:28 +0530

    * brand CFP for RubyConf India 2026

--- app/frontend/components/Footer.tsx
+++ app/frontend/components/Footer.tsx
@@ -5,10 +5,15 @@ export function Footer() {
     <footer className="bg-secondary-800 text-cloud-500 sticky bottom-0 w-full py-8">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="border-secondary-400 mt-8 flex flex-col items-center justify-between border-t pt-6 md:flex-row">
-          {/* FIXME: Add your conference name */}
           <p className="text-secondary-400 text-sm">
-            &copy; {currentYear} Example Conference
+            &copy; {currentYear} RubyConf India
           </p>
+          <a
+            href="https://rubyconf.in"
+            className="text-secondary-400 hover:text-cloud-200 text-sm"
+          >
+            rubyconf.in
+          </a>
         </div>
       </div>
     </footer>
